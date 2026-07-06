75f368a62b6a1f3c7659c81fefe7a3a21f2217be
Alexander Ott <66271487+AlexanderHott@users.noreply.github.com>
2026-07-05 22:38:17 -0700

    clean up starting template

--- src/raylib_game.c
+++ src/raylib_game.c
@@ -1,29 +1,16 @@
-/*******************************************************************************************
-*
-*   raylib gamejam template
-*
-*   Code licensed under an unmodified zlib/libpng license, which is an OSI-certified,
-*   BSD-like license that allows static linking with closed source software
-*
-*   Copyright (c) 2022-2026 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
-
 #include "raylib.h"
 
 #if defined(PLATFORM_WEB)
-    #include <emscripten/emscripten.h>      // Emscripten library
+    #include <emscripten/emscripten.h>
 #endif
 
-#include <stdio.h>                          // Required for: printf()
-#include <stdlib.h>                         // Required for: 
-#include <string.h>                         // Required for:
+#include <assert.h> 
+#include <stdio.h> 
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <stddef.h>
 
-//----------------------------------------------------------------------------------
-// Defines and Macros
-//----------------------------------------------------------------------------------
-// Simple log system to avoid printf() calls if required
-// NOTE: Avoiding those calls, also avoids const strings memory usage
 #define SUPPORT_LOG_INFO
 #if defined(SUPPORT_LOG_INFO)
     #define LOG(...) printf(__VA_ARGS__)
@@ -31,9 +18,45 @@
     #define LOG(...)
 #endif
 
-//----------------------------------------------------------------------------------
-// Types and Structures Definition
-//----------------------------------------------------------------------------------
+#define ASSERT(condition, message) assert(condition && message)
+
+#define STATIC_ASSERT_JOIN2(a, b) a##b
+#define STATIC_ASSERT_JOIN(a, b) STATIC_ASSERT_JOIN2(a, b)
+#define STATIC_ASSERT(condition, message) \
+    typedef char STATIC_ASSERT_JOIN(static_assertion_failed__##message##__line_, __LINE__)[(condition) ? 1 : -1]
+
+#define UNUSED(identifier) ((void)identifier)
+
+typedef uint8_t   u8;
+typedef uint16_t  u16;
+typedef uint32_t  u32;
+typedef uint64_t  u64;
+
+typedef int8_t    i8;
+typedef int16_t   i16;
+typedef int32_t   i32;
+typedef int64_t   i64;
+
+typedef float     f32;
+typedef double    f64;
+
+STATIC_ASSERT(sizeof(u8)  == 1, u8_is_not_1_byte);
+STATIC_ASSERT(sizeof(u16) == 2, u16_is_not_2_bytes);
+STATIC_ASSERT(sizeof(u32) == 4, u32_is_not_4_bytes);
+STATIC_ASSERT(sizeof(u64) == 8, u64_is_not_8_bytes);
+
+STATIC_ASSERT(sizeof(i8)  == 1, i8_is_not_1_byte);
+STATIC_ASSERT(sizeof(i16) == 2, i16_is_not_2_bytes);
+STATIC_ASSERT(sizeof(i32) == 4, i32_is_not_4_bytes);
+STATIC_ASSERT(sizeof(i64) == 8, i64_is_not_8_bytes);
+
+STATIC_ASSERT(sizeof(f32) == 4, f32_is_not_4_bytes);
+STATIC_ASSERT(sizeof(f64) == 8, f32_is_not_8_bytes);
+
+////////////////////////////////////////////////////////////////////////////////
+/// Game
+////////////////////////////////////////////////////////////////////////////////
+
 typedef enum { 
     SCREEN_LOGO = 0, 
     SCREEN_TITLE, 
@@ -41,91 +64,48 @@ typedef enum {
     SCREEN_ENDING
 } GameScreen;
 
-// TODO: Define your custom data types here
+static const u32 screenWidth = 720;
+static const u32 screenHeight = 720;
 
-//----------------------------------------------------------------------------------
-// Global Variables Definition (local to this module)
-//----------------------------------------------------------------------------------
-static const int screenWidth = 720;
-static const int screenHeight = 720;
+static RenderTexture2D target = { 0 };
+static u32 frameCounter = 0;
 
-static RenderTexture2D target = { 0 };  // Render texture to render our game
-static int frameCounter = 0;
+static void UpdateDrawFrame(void);
 
-// TODO: Define global variables here, recommended to make them static
-
-//----------------------------------------------------------------------------------
-// Module Functions Declaration
-//----------------------------------------------------------------------------------
-static void UpdateDrawFrame(void);      // Update and Draw one frame
-
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
-int main(void)
+i32 main(void)
 {
 #if !defined(_DEBUG)
-    SetTraceLogLevel(LOG_NONE);         // Disable raylib trace log messages
+    SetTraceLogLevel(LOG_NONE);
 #endif
-
-    // Initialization
-    //--------------------------------------------------------------------------------------
     InitWindow(screenWidth, screenHeight, "raylib gamejam template");
     
-    // TODO: Load resources / Initialize variables at this point
-    
-    // Render texture to draw, enables screen scaling
-    // NOTE: If screen is scaled, mouse input should be scaled proportionally
     target = LoadRenderTexture(screenWidth, screenHeight);
     SetTextureFilter(target.texture, TEXTURE_FILTER_BILINEAR);
 
 #if defined(PLATFORM_WEB)
     emscripten_set_main_loop(UpdateDrawFrame, 60, 1);
 #else
-    SetTargetFPS(60);     // Set our game frames-per-second
-    //--------------------------------------------------------------------------------------
-
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button
+    SetTargetFPS(60);
+    while (!WindowShouldClose())
     {
         UpdateDrawFrame();
     }
 #endif
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
     UnloadRenderTexture(target);
     
-    // TODO: Unload all loaded resources at this point
-
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
-
+    CloseWindow();
     return 0;
 }
 
-//--------------------------------------------------------------------------------------------
-// Module Functions Definition
-//--------------------------------------------------------------------------------------------
-// Update and draw frame
 void UpdateDrawFrame(void)
 {
-    // Update
-    //----------------------------------------------------------------------------------
-    // TODO: Update variables / Implement example logic at this point
-   
     frameCounter++;
-    //----------------------------------------------------------------------------------
 
-    // Draw
-    //----------------------------------------------------------------------------------
-    // Render game screen to a texture, 
-    // it could be useful for scaling or further shader postprocessing
     BeginTextureMode(target);
+    {
         ClearBackground(RAYWHITE);
         
-        // TODO: Draw your game screen here
-
         DrawRectangle(70, 90, 200, 200, BLACK);
         DrawRectangle(70 + 16, 90 + 16, 200 - 32, 200 - 32, RAYWHITE);
         DrawText("raylib", 70 + 200 - MeasureText("raylib", 40) - 32, 90 + 200 - 40 - 24, 40, BLACK);
@@ -136,19 +116,16 @@ void UpdateDrawFrame(void)
         if ((frameCounter/20)%2) DrawText("are you ready?", 160, 500, 50, BLACK);
         
         DrawRectangleLinesEx((Rectangle){ 0, 0, screenWidth, screenHeight }, 16, BLACK);
-        
+    }
     EndTextureMode();
     
-    // Render to screen (main framebuffer)
     BeginDrawing();
+    {
         ClearBackground(RAYWHITE);
         
-        // Draw render texture to screen, scaled if required
-        DrawTexturePro(target.texture, (Rectangle){ 0, 0, (float)target.texture.width, -(float)target.texture.height }, 
-            (Rectangle){ 0, 0, (float)target.texture.width, (float)target.texture.height }, (Vector2){ 0, 0 }, 0.0f, WHITE);
+        DrawTexturePro(target.texture, (Rectangle){ 0, 0, (f32)target.texture.width, -(f32)target.texture.height }, 
+            (Rectangle){ 0, 0, (f32)target.texture.width, (f32)target.texture.height }, (Vector2){ 0, 0 }, 0.0f, WHITE);
 
-        // TODO: Draw everything that requires to be drawn at this point, maybe UI?
-
+    }
     EndDrawing();
-    //----------------------------------------------------------------------------------  
 }
